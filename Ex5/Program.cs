a14af83a81318c6cc70bda54b52a201ada6cbb22
Nataliia <nataliyatrishechkina@mail.ru>
2022-11-21 17:14:54 +0300

    ex5

--- Ex5/Program.cs
+++ Ex5/Program.cs
@@ -1,12 +1,1 @@
-﻿Console.WriteLine("Введите имя пользователя");
-string username = Console.ReadLine();
-
-if (username = Маша);
-{
-    Console.WriteLine("Ура, это же МАША");
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);
-}
+﻿
